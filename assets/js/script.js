ba84173252fcb415782b5f98e7f9a19828a47936
Todd Malone <toddomalone@gmail.com>
2021-01-28 19:19:38 -0600

    added onclick for two input fields (ingredients and cook time)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,22 @@ var edamamAPIKey = '3bad439961e3fa77b543342040638031';
 
 function getRecipieList(searchTerm,cookTime){
 
-    var queryURL = "https://chriscastle.com/proxy/index.php?:proxy:https://api.edamam.com/search?q="+searchTerm+"&app_id="+edamamAPIid+"&app_key="+edamamAPIKey+"&from=0&to=5"
+    var proxy = "https://chriscastle.com/proxy/index.php?:proxy:"
+    var queryURL = "https://api.edamam.com/search?q="+searchTerm+"&app_id="+edamamAPIid+"&app_key="+edamamAPIKey+"&from=0&to=5"
 
     $.ajax({
         method:'GET',
-        url:queryURL
+        url:proxy+queryURL
     }).then(function(data){
+        // console.log(data)
         console.log(JSON.parse(data));
     })
 }
+
+
+$("#searchButton").on("click",function(){
+    cookTime = $("#cook-time").val()=="" ? "" : $("#cook-time").val()
+
+    getRecipieList($("#ingredient-filter").val(),cookTime);   
+
+})

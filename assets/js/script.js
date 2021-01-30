11c05d1e4c71588004512b6ddb906733a5cafe82
Justin Rovnak <jmrovnak@gmail.com>
2021-01-30 12:29:50 -0600

    added variable for search term, empty divs on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ var edamamAPIid = '556477a6';
 var edamamAPIKey = '3bad439961e3fa77b543342040638031';
 var spoonacularAPIKey ='8f3601ecf03246c9b4fa1b4a254023c5';
 
+var searchTerm = $("#searchButton").val().trim();
+var proxy = "https://chriscastle.com/proxy/index.php?:proxy:"
+var edamamQueryURL = "https://api.edamam.com/search?q="+searchTerm+"&app_id="+edamamAPIid+"&app_key="+edamamAPIKey+"&from=0&to=5"
+var spoonacularQueryURL = "https://api.spoonacular.com/recipes/complexSearch?q="+searchTerm+"&includeNutrition=true&instructionsRequired=true&apiKey="+spoonacularAPIKey
+
 //https://api.spoonacular.com/recipes/716429/information?apiKey=8f3601ecf03246c9b4fa1b4a254023c5&includeNutrition=true
 
 function getRecipeList(searchTerm,cookTime){
@@ -18,6 +23,8 @@ function getRecipeList(searchTerm,cookTime){
     }).then(function(data){
         console.log(data)
         // console.log(JSON.parse(data));
+
+       
     })
 }
 
@@ -56,6 +63,16 @@ $("#searchButton").on("click",function(){
     cookTime = $("#cook-time").val()=="" ? "" : $("#cook-time").val()
 
     getRecipeList($("#ingredient-filter").val(),cookTime);   
+    console.log("test");
+    console.log(spoonacularQueryURL);
+
+    $(".card-title").empty();
+    $(".line").empty();
+    $(".vitamins").empty();
+
+    $("#main-recipe").html("populate clickable recipes here");
+
+
       
 })
 
